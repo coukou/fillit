c9c675a66489c93740319c0f3eda93c0ac29e5b1
Benoit GEORGES <bgeorges@e1r13p14.42.fr>
2017-12-08 18:45:52 +0100

    test1

--- fillit.c
+++ fillit.c
@@ -6,54 +6,47 @@
 /*   By: bgeorges <bgeorges@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/30 16:56:33 by bgeorges          #+#    #+#             */
-/*   Updated: 2017/12/08 16:34:00 by bgeorges         ###   ########.fr       */
+/*   Updated: 2017/12/08 18:45:20 by bgeorges         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <fcntl.h>
 #include "libft.h"
+#define BUFF_SIZE 4096
 
-#define	BUFF_SIZE 4096
-
-void	ft_convert(char *tetriminos, int nb_tetriminos)
+void		ft_convert(char *tetriminos, int nb_tetriminos)
 {
-	int i;
-	unsigned short	converted;
+	int				i;
+	unsigned short	tetriminos_converted;
 	unsigned short	all_converted[26];
 
 	i = 0;
-	while(tetriminos[i] != '\0')
+	while (tetriminos[i] != '\0')
 	{
-		converted = converted  << 1;
+		tetriminos_converted = tetriminos_converted << 1;
 		if (tetriminos[i] == '#')
-			converted |= 1;
+			tetriminos_converted |= 1;
 		i++;
 	}
-	all_converted[nb_tetriminos] = converted;
-	printf("%d\n", all_converted[nb_tetriminos]);
+	all_converted[nb_tetriminos] = tetriminos_converted;
 }
 
-void	ft_error()
+void	ft_error(void)
 {
 	ft_putendl("error");
 	exit(EXIT_FAILURE);
 }
 
-void	ft_valid2(char *tetriminos, int nb_tetriminos)
+void		ft_validate_chars(char *tetriminos, int nb_tetriminos)
 {
-	int right;
-	int left;
-	int top;
-	int bottom;
-	int i;
-	int j;
+	int		right;
+	int		left;
+	int		top;
+	int		bottom;
+	int		i;
 
 	i = 0;
-	j = 0;
 	while (tetriminos[i] != '\0')
 	{
 		if (tetriminos[i] == '#')
@@ -62,35 +55,31 @@ void	ft_valid2(char *tetriminos, int nb_tetriminos)
 			left = (i - 1) >= 0 ? tetriminos[i - 1] == '#' : 0;
 			top = (i - 4) >= 0 ? tetriminos[i - 4] == '#' : 0;
 			bottom = (i + 4) <= 16 ? tetriminos[i + 4] == '#' : 0;
-
-		if (right == 0 && left == 0 && top == 0 && bottom == 0)
-			ft_error();
+			if (right == 0 && left == 0 && top == 0 && bottom == 0)
+				ft_error();
 		}
 		i++;
 	}
-		ft_convert(tetriminos, nb_tetriminos);
+	ft_convert(tetriminos, nb_tetriminos);
 }
 
-int ft_valid1(char *buf)
+void		ft_vaidate_tetriminos(char *buf)
 {
-	int i;
-	int j;
-	int k;
-	int nb_tetriminos;
-	char tetriminos[16];
+	int		i;
+	int		j;
+	int		k;
+	int		nb_tetriminos;
+	char	tetriminos[16];
 
 	i = 0;
 	k = 0;
 	nb_tetriminos = 0;
 	while (buf[k] != '\0')
 	{
-
 		if (i == '\n')
-		{
 			ft_error();
-		}
 		j = 0;
-		while ((buf[k] == '.' || buf[k] == '#') && buf[k] != '\n')
+		while (buf[k] == '.' || buf[k] == '#')
 		{
 			tetriminos[i] = buf[k];
 			j++;
@@ -98,29 +87,25 @@ int ft_valid1(char *buf)
 			i++;
 		}
 		if (j != 4)
-		{
 			ft_error();
-		}
-		else
-			k++;
-		if (i == 16 || buf[k] == '\0')
+		k++;
+		if (i == 16)
 		{
 			tetriminos[i] = '\0';
-			ft_valid2(tetriminos, nb_tetriminos);
+			ft_validate_chars(tetriminos, nb_tetriminos);
 			nb_tetriminos++;
 			if (buf[k] == '\n')
 				k++;
 			i = 0;
 		}
 	}
-	return(0);
 }
 
-void		ft_isvalid(int argc, char **argv)
+void		ft_isvalid(int ac, char **av)
 {
-	char buf[BUFF_SIZE + 1];
-	int fd;
-	int ret;
+	char	buf[BUFF_SIZE + 1];
+	int		fd;
+	int		ret;
 
 	if (argc != 2)
 	{
@@ -132,10 +117,10 @@ void		ft_isvalid(int argc, char **argv)
 		ft_error();
 	ret = read(fd, buf, BUFF_SIZE);
 	buf[ret] = '\0';
-	ft_valid1(buf);
-	}
+	ft_vaidate_tetriminos(buf);
+}
 
-int main (int ac, char **av)
+int			main(int ac, char **av)
 {
 	ft_isvalid(ac, av);
 	return (0);
